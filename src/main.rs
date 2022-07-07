48fbc399e663885518b0d0892aa69012d1a88c4a
Nicolai Holmgaard <Nicolai@holmgaard.info>
2022-07-07 11:16:51 +0200

    updated
    got some comments on my code, now its fixed and is way better

--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,66 @@
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::BufReader;
+use std::fs;
+// use std::io::prelude::*;
+// use std::io::BufReader;
 use std::io::stdin;
 use std::process;
 use rand::Rng;
 
 fn main() {
-    let words: Vec<String> = readfile("src/words.txt");
-    
+    let lines = fs::read_to_string("src/words.txt").expect("Couldn't open file!");
+    let words: Vec<&str> = lines.lines().collect();
+
     let rand_num = rand::thread_rng().gen_range(0..words.len());
-    let selectedword: &String = &words.get(rand_num).unwrap();
+    let selectedword = &words[rand_num];
 
 
     println!("Welcome to my cli wordle clone made in rust");
     println!("Go ahead, take your first guess");
 
-    let mut i = 1;
-
-    while i < 6 {
-        if round(&words, &selectedword) {
+    for i in 1..7 {
+        if round(words.as_slice(), selectedword) {
             println!("Yay you did it, you guessed right. The word was {}!", selectedword);
             println!("You did it {} guess!", &i);
             process::exit(1);
         }
-        i = i+1;
     }
     println!("You didn't make it this time. Better luck next time!")
 
 }
 
-fn readfile(path: &str) -> Vec<String>{
-    let file = File::open(path)
-        .expect("Cound't find the file!");
-    let buf_reader = BufReader::new(file);
 
-    let mut output: Vec<String> = Vec::new();
-
-    for line in buf_reader.lines() {
-        output.push(line.unwrap());
-    }
-    
-    return output;
-}
-
-fn round(words: &Vec<String>, selectedword: &String) -> bool{
-    let mut input_string = String::new();
+fn round(words: &[&str], selectedword: &str) -> bool{
+    let mut input_string = String::with_capacity(5);
     stdin().read_line(&mut input_string)
-    	.ok()
         .expect("Failed to read line");
 
-    input_string.remove(input_string.len()- 1);
-    input_string.remove(input_string.len()- 1);
+    let input_string = input_string.trim();
     
-
 
     if !input_string.len() == 5{
         println!("{} is the wrong length, try again", input_string);
         return round(words, &selectedword)
     }
 
-    // input_string.remove(idx)
-    // input_string.remove_matches("\n");
-
     
     if !words.contains(&input_string){
         println!("{} is not a valid word, try again", input_string);
         return round(words, &selectedword)
     }
-    
-    let mut i = 0;
 
-    for char in input_string.chars() {
-        if char == selectedword.chars().nth(i).unwrap(){
+    let correct_word = selectedword.chars().collect::<Vec<char>>();
+
+
+    for (i, c) in input_string.chars().enumerate() {
+        if c == correct_word[i] {
             print!("🟩");
-        } else if selectedword.contains(char){
+        } else if selectedword.contains(c) {
             print!("🟨");
         } else {
             print!("⬛");
         }
-        i = i+1;
     }
+
     println!(" ");
 
-    if input_string == selectedword.to_string() {
-        return true;
-    } else {
-        return false;
-    }
+    input_string == selectedword
 }
